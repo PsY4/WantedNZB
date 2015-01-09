ab0b77aa3664d02eea26a702651b35f19107d819
PsY4 <alex@alexetaurore.com>
2015-01-09 15:02:34 +0100

    ... oubli ...

--- nzb_get.php
+++ nzb_get.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set("display_errors", 1);
 include_once('simple_html_dom.php');
+include_once('config.php');
 
 // ************************************
 // PARAMS du SCRIPT
